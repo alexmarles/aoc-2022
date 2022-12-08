c209add515fef9e776a79ff74bf10e216b3c2adc
Alex Marles Alcaraz <alex.marles@king.com>
2022-12-08 17:29:19 +0100

    Day 08 - Second puzzle

--- day08/day08.js
+++ day08/day08.js
@@ -40,6 +40,61 @@ function day08A(file) {
     return visibleCount;
 }
 
+function day08B(file) {
+    const data = getInputData(file).map(row =>
+        row.split('').map(n => Number(n))
+    );
+    const scores = {};
+
+    data.forEach((row, y) => {
+        if (y === 0 || y === data.length - 1) return; // Skip edges
+        row.forEach((el, x) => {
+            if (x === 0 || x === row.length - 1) return; // Skip edges
+            const rowL = row.slice(0, x);
+            const rowR = row.slice(x + 1, row.length);
+            let scoreL = 0;
+            let scoreR = 0;
+
+            while (scoreL < x && el > rowL[rowL.length - 1 - scoreL]) scoreL++;
+            scoreL += scoreL < x ? 1 : 0;
+
+            while (scoreR < rowR.length && el > rowR[scoreR]) scoreR++;
+            scoreR += scoreR < rowR.length ? 1 : 0;
+
+            if (scores[y] === undefined) scores[y] = {};
+            scores[y][x] = scoreL * scoreR;
+        });
+    });
+
+    const transposed = transpose(data);
+    transposed.forEach((col, x) => {
+        if (x === 0 || x === transposed.length - 1) return; // Skip edges
+        col.forEach((el, y) => {
+            if (y === 0 || y === col.length - 1) return; // Skip edges
+            const colU = col.slice(0, y);
+            const colD = col.slice(y + 1, col.length);
+            let scoreU = 0;
+            let scoreD = 0;
+
+            while (scoreU < y && el > colU[colU.length - 1 - scoreU]) scoreU++;
+            scoreU += scoreU < y ? 1 : 0;
+
+            while (scoreD < colD.length && el > colD[scoreD]) scoreD++;
+            scoreD += scoreD < colD.length ? 1 : 0;
+
+            scores[y][x] *= scoreU * scoreD;
+        });
+    });
+
+    const result = Object.values(scores)
+        .map(s => Object.values(s))
+        .join()
+        .split(',')
+        .map(n => Number(n));
+    return max(result);
+}
+
 module.exports = {
     day08A,
+    day08B,
 };
--- day08/day08.test.js
+++ day08/day08.test.js
@@ -1,4 +1,4 @@
-const { day08A } = require('./day08');
+const { day08A, day08B } = require('./day08');
 
 describe('Day 08 A', () => {
     test('should solve the example', () => {
@@ -11,5 +11,19 @@ describe('Day 08 A', () => {
         const result = day08A('day08/inputData');
 
         expect(result).toBe(1543);
+    });
+});
+
+describe('Day 08 B', () => {
+    test('should solve the example', () => {
+        const result = day08B('day08/exampleData');
+
+        expect(result).toBe(8);
+    });
+
+    test('should solve the puzzle', () => {
+        const result = day08B('day08/inputData');
+
+        expect(result).toBe(595080);
     });
 });
