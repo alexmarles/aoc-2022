8401eb07dcf1084bf3944c16e240ecff00de79fd
Alex Marles Alcaraz <alex.marles@king.com>
2022-12-10 15:39:49 +0100

    Day 10 - Second puzzle

--- day10/day10.js
+++ day10/day10.js
@@ -38,6 +38,54 @@ function day10A(file) {
     return sum(strengths.map(s => s.strenght));
 }
 
+function day10B(file) {
+    const instructions = getInputData(file);
+
+    const LIT = '#';
+    const DARK = '.';
+    const CRT = [...new Array(6)].map(() => [...new Array(40)]);
+    let x = 1;
+    let cycle = 1;
+
+    function printScreen() {
+        return CRT.map(line => line.join('')).join('\n');
+    }
+
+    function startCycle() {
+        const row = Math.floor((cycle - 1) / INTERVAL);
+        const col = (cycle - 1) % INTERVAL;
+        if ([x - 1, x, x + 1].includes(col)) {
+            CRT[row][col] = LIT;
+        } else {
+            CRT[row][col] = DARK;
+        }
+    }
+
+    function endCycle() {
+        cycle++;
+    }
+
+    function endOpCycle(v) {
+        x = x + v;
+        cycle++;
+    }
+
+    instructions.forEach(instruction => {
+        const [op, rawValue] = instruction.split(' ');
+        const v = Number(rawValue) || null;
+
+        startCycle();
+        endCycle();
+        if (op === 'noop') return;
+
+        startCycle();
+        endOpCycle(v);
+    });
+
+    return printScreen();
+}
+
 module.exports = {
     day10A,
+    day10B,
 };
