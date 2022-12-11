24fca0060de66fc6d00def30e434a0c57f70fe4e
Alex Marles Alcaraz <alex.marles@king.com>
2022-12-11 16:09:47 +0100

    Refactor Day 11

--- day11/day11.js
+++ day11/day11.js
@@ -6,24 +6,40 @@ const gcd = (a, b) => (a ? gcd(b % a, a) : b);
 
 const lcm = (a, b) => (a * b) / gcd(a, b);
 
-function parseMonkey(monkey) {
-    const items = monkey
+function parseMonkey(raw) {
+    const monkey = {};
+    monkey.inspected = 0;
+    monkey.items = raw
         .match(/^\s*Starting\sitems: ([0-9,\s]*)$/m)[1]
         .split(',')
         .map(i => Number(i));
-    const op = old => eval(monkey.match(/^\s*Operation:\snew\s=\s(.*)$/m)[1]);
-    const test = Number(monkey.match(/^\s*Test.*\s([0-9]*)$/m)[1]);
-    const ifTrue = Number(monkey.match(/^\s*If\strue.*\s([0-9])/m)[1]);
-    const ifFalse = Number(monkey.match(/^\s*If\sfalse.*\s([0-9])/m)[1]);
+    monkey.op = old => eval(raw.match(/^\s*Operation:\snew\s=\s(.*)$/m)[1]);
+    monkey.test = Number(raw.match(/^\s*Test.*\s([0-9]*)$/m)[1]);
+    monkey.ifTrue = Number(raw.match(/^\s*If\strue.*\s([0-9])/m)[1]);
+    monkey.ifFalse = Number(raw.match(/^\s*If\sfalse.*\s([0-9])/m)[1]);
 
-    return {
-        items,
-        op,
-        test,
-        ifTrue,
-        ifFalse,
-        inspected: 0,
-    };
+    return monkey;
+}
+
+function monkeysTurn({ monkey, allMonkeys, relief = false, factor = false }) {
+    monkey.items.forEach(item => {
+        let worry = monkey.op(item);
+        if (relief) worry = Math.floor(worry / relief);
+        else worry = worry % factor;
+        const { ifTrue, ifFalse } = monkey;
+        const to = worry % monkey.test === 0 ? ifTrue : ifFalse;
+        allMonkeys[to].items.push(worry);
+        monkey.inspected++;
+    });
+    monkey.items = [];
+}
+
+function monkeyBusiness(arr) {
+    return arr
+        .map(m => m.inspected)
+        .sort((a, b) => b - a)
+        .slice(0, 2)
+        .reduce((acc, curr) => acc * curr, 1);
 }
 
 function day11A(file) {
@@ -32,29 +48,17 @@ function day11A(file) {
     const data = getInputDataInChunks(file);
     const monkeys = data.map(parseMonkey);
 
-    function monkeysTurn(monkey) {
-        monkey.items.forEach(item => {
-            let worry = monkey.op(item);
-            worry = Math.floor(worry / RELIEF);
-            const to =
-                worry % monkey.test === 0 ? monkey.ifTrue : monkey.ifFalse;
-            monkeys[to].items.push(worry);
-            monkey.inspected++;
-        });
-        monkey.items = [];
-    }
-
     for (let i = 0; i < ROUNDS; i++) {
-        monkeys.forEach(monkeysTurn);
+        monkeys.forEach(monkey => {
+            monkeysTurn({
+                monkey,
+                allMonkeys: monkeys,
+                relief: RELIEF,
+            });
+        });
     }
 
-    const top2 = monkeys
-        .map(m => m.inspected)
-        .sort((a, b) => b - a)
-        .slice(0, 2);
-    const monkeyBusiness = top2.reduce((acc, curr) => acc * curr, 1);
-
-    return monkeyBusiness;
+    return monkeyBusiness(monkeys);
 }
 
 function day11B(file) {
@@ -63,25 +67,17 @@ function day11B(file) {
     const monkeys = data.map(parseMonkey);
     const factor = monkeys.map(m => m.test).reduce(lcm);
 
-    function monkeysTurn(monkey) {
-        monkey.items.forEach((item, i) => {
-            let worry = monkey.op(item);
-            worry = worry % factor;
-            const { ifTrue, ifFalse } = monkey;
-            const to = worry % monkey.test === 0 ? ifTrue : ifFalse;
-            monkeys[to].items.push(worry);
-            monkey.inspected++;
+    for (let i = 0; i < ROUNDS; i++) {
+        monkeys.forEach(monkey => {
+            monkeysTurn({
+                monkey,
+                allMonkeys: monkeys,
+                factor,
+            });
         });
-        monkey.items = [];
     }
 
-    for (let i = 0; i < ROUNDS; i++) monkeys.forEach(monkeysTurn);
-
-    return monkeys
-        .map(m => m.inspected)
-        .sort((a, b) => b - a)
-        .slice(0, 2)
-        .reduce((acc, curr) => acc * curr, 1);
+    return monkeyBusiness(monkeys);
 }
 
 module.exports = {
