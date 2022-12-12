ca3bd2dda3df9e48f398b6c1c484e48d5516432b
Alex Marles Alcaraz <alex.marles@king.com>
2022-12-12 20:44:06 +0100

    Refactor Day 12

--- day12/day12.js
+++ day12/day12.js
@@ -3,26 +3,25 @@
 const { getInputData } = require('../utils');
 const dijkstra = require('dijkstrajs');
 
-function day12A(file) {
-    const data = getInputData(file);
-    const heightMap = data.map(r => r.split('').map(p => p.charCodeAt() - 96));
-    let start = {};
-    let end = {};
-    heightMap.forEach((row, y) => {
-        row.forEach((col, x) => {
-            if (col === 'S'.charCodeAt() - 96) {
-                start = { x, y };
-                heightMap[y][x] = 'a'.charCodeAt() - 96;
-            }
-            if (col === 'E'.charCodeAt() - 96) {
-                end = { x, y };
-                heightMap[y][x] = 'z'.charCodeAt() - 96;
-            }
-        });
-    });
+function getAllTrails(input) {
+    const heightMap = input.map(r =>
+        r
+            .replace('S', 'a')
+            .replace('E', 'z')
+            .split('')
+            .map(p => p.charCodeAt() - 96)
+    );
+    const end = {
+        x: input.join('').indexOf('E') % input[0].length,
+        y: Math.floor(input.join('').indexOf('E') / input[0].length),
+    };
+    const starts = [];
+
     const graph = {};
     heightMap.forEach((row, y) => {
         row.forEach((col, x) => {
+            if (col === 1) starts.push({ x, y });
+
             const coords = `${x}_${y}`;
             graph[coords] = {};
             if (heightMap[y - 1]) {
@@ -47,73 +46,40 @@ function day12A(file) {
             }
         });
     });
-    const path = dijkstra.find_path(
-        graph,
-        `${start.x}_${start.y}`,
-        `${end.x}_${end.y}`
-    );
-    return path.length - 1;
+
+    const trails = new Map();
+    starts.forEach(start => {
+        try {
+            const path = dijkstra.find_path(
+                graph,
+                `${start.x}_${start.y}`,
+                `${end.x}_${end.y}`
+            );
+            trails.set(`${start.x}_${start.y}`, path.length - 1);
+        } catch {
+            trails.set(`${start.x}_${start.y}`, Number.MAX_VALUE);
+        }
+    });
+
+    return trails;
+}
+
+function day12A(file) {
+    const data = getInputData(file);
+    const trails = getAllTrails(data);
+    const start = {
+        x: data.join('').indexOf('S') % data[0].length,
+        y: Math.floor(data.join('').indexOf('S') / data[0].length),
+    };
+
+    return trails.get(`${start.x}_${start.y}`);
 }
 
 function day12B(file) {
     const data = getInputData(file);
-    const heightMap = data.map(r => r.split('').map(p => p.charCodeAt() - 96));
-    const starts = [];
-    let end = {};
-    heightMap.forEach((row, y) => {
-        row.forEach((col, x) => {
-            if (col === 1) starts.push({ x, y });
-            if (col === 'S'.charCodeAt() - 96) {
-                starts.push({ x, y });
-                heightMap[y][x] = 'a'.charCodeAt() - 96;
-            }
-            if (col === 'E'.charCodeAt() - 96) {
-                end = { x, y };
-                heightMap[y][x] = 'z'.charCodeAt() - 96;
-            }
-        });
-    });
-    const graph = {};
-    heightMap.forEach((row, y) => {
-        row.forEach((col, x) => {
-            const coords = `${x}_${y}`;
-            graph[coords] = {};
-            if (heightMap[y - 1]) {
-                const newCoords = `${x}_${y - 1}`;
-                const diff = heightMap[y - 1][x] - col;
-                if (diff <= 1) graph[coords][newCoords] = diff + 1;
-            }
-            if (heightMap[y + 1]) {
-                const newCoords = `${x}_${y + 1}`;
-                const diff = heightMap[y + 1][x] - col;
-                if (diff <= 1) graph[coords][newCoords] = diff + 1;
-            }
-            if (heightMap[y][x - 1]) {
-                const newCoords = `${x - 1}_${y}`;
-                const diff = heightMap[y][x - 1] - col;
-                if (diff <= 1) graph[coords][newCoords] = diff + 1;
-            }
-            if (heightMap[y][x + 1]) {
-                const newCoords = `${x + 1}_${y}`;
-                const diff = heightMap[y][x + 1] - col;
-                if (diff <= 1) graph[coords][newCoords] = diff + 1;
-            }
-        });
-    });
-    const paths = starts.map(start => {
-        try {
-            return (
-                dijkstra.find_path(
-                    graph,
-                    `${start.x}_${start.y}`,
-                    `${end.x}_${end.y}`
-                ).length - 1
-            );
-        } catch {
-            return Number.MAX_VALUE;
-        }
-    });
-    return Math.min.apply(Math, paths);
+    const trails = getAllTrails(data);
+
+    return Math.min.apply(Math, [...trails.values()]);
 }
 
 module.exports = {
