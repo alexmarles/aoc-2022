ea3a2544becf6c298406a38307790278aa8bd7d0
Alex Marles Alcaraz <alex.marles@king.com>
2022-12-12 18:57:57 +0100

    Day 12 - Second puzzle

--- day12/day12.js
+++ day12/day12.js
@@ -55,6 +55,68 @@ function day12A(file) {
     return path.length - 1;
 }
 
+function day12B(file) {
+    const data = getInputData(file);
+    const heightMap = data.map(r => r.split('').map(p => p.charCodeAt() - 96));
+    const starts = [];
+    let end = {};
+    heightMap.forEach((row, y) => {
+        row.forEach((col, x) => {
+            if (col === 1) starts.push({ x, y });
+            if (col === 'S'.charCodeAt() - 96) {
+                starts.push({ x, y });
+                heightMap[y][x] = 'a'.charCodeAt() - 96;
+            }
+            if (col === 'E'.charCodeAt() - 96) {
+                end = { x, y };
+                heightMap[y][x] = 'z'.charCodeAt() - 96;
+            }
+        });
+    });
+    const graph = {};
+    heightMap.forEach((row, y) => {
+        row.forEach((col, x) => {
+            const coords = `${x}_${y}`;
+            graph[coords] = {};
+            if (heightMap[y - 1]) {
+                const newCoords = `${x}_${y - 1}`;
+                const diff = heightMap[y - 1][x] - col;
+                if (diff <= 1) graph[coords][newCoords] = diff + 1;
+            }
+            if (heightMap[y + 1]) {
+                const newCoords = `${x}_${y + 1}`;
+                const diff = heightMap[y + 1][x] - col;
+                if (diff <= 1) graph[coords][newCoords] = diff + 1;
+            }
+            if (heightMap[y][x - 1]) {
+                const newCoords = `${x - 1}_${y}`;
+                const diff = heightMap[y][x - 1] - col;
+                if (diff <= 1) graph[coords][newCoords] = diff + 1;
+            }
+            if (heightMap[y][x + 1]) {
+                const newCoords = `${x + 1}_${y}`;
+                const diff = heightMap[y][x + 1] - col;
+                if (diff <= 1) graph[coords][newCoords] = diff + 1;
+            }
+        });
+    });
+    const paths = starts.map(start => {
+        try {
+            return (
+                dijkstra.find_path(
+                    graph,
+                    `${start.x}_${start.y}`,
+                    `${end.x}_${end.y}`
+                ).length - 1
+            );
+        } catch {
+            return Number.MAX_VALUE;
+        }
+    });
+    return Math.min.apply(Math, paths);
+}
+
 module.exports = {
     day12A,
+    day12B,
 };
--- day12/day12.test.js
+++ day12/day12.test.js
@@ -1,4 +1,4 @@
-const { day12A } = require('./day12');
+const { day12A, day12B } = require('./day12');
 
 describe('Day 12 A', () => {
     test('should solve the example', () => {
@@ -11,5 +11,19 @@ describe('Day 12 A', () => {
         const result = day12A('day12/inputData');
 
         expect(result).toBe(339);
+    });
+});
+
+describe('Day 12 B', () => {
+    test('should solve the example', () => {
+        const result = day12B('day12/exampleData');
+
+        expect(result).toBe(29);
+    });
+
+    test('should solve the puzzle', () => {
+        const result = day12B('day12/inputData');
+
+        expect(result).toBe(332);
     });
 });
