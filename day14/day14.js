cdf19bf1a73a77b3e3167656bf2031d0732287e6
Alex Marles Alcaraz <alex.marles@king.com>
2022-12-14 22:44:45 +0100

    Refactor Day 14 first puzzle

--- day14/day14.js
+++ day14/day14.js
@@ -4,10 +4,57 @@ const { getInputData } = require('../utils');
 
 const ROCK = '#';
 const SAND = 'o';
-const AIR = '.';
+const AIR = ' ';
+const START = [500, 0];
 
 function printCave(cave) {
-    return cave.map((row, i) => [i, ...row].join('')).join('\n');
+    const blueprint = [];
+    for (let i = 0; i < cave.get('depth'); i++) {
+        blueprint[i] = [];
+        for (let j = cave.get('minWidth'); j <= cave.get('maxWidth'); j++) {
+            blueprint[i][j] = cave.get(`${j}_${i}`) || AIR;
+        }
+    }
+
+    return blueprint.map((row, i) => [...row].join('')).join('\n');
+}
+
+function getSandPouredIn({ cave, hasFloor = false }) {
+    const depth = cave.get('depth');
+    const minWidth = cave.get('minWidth');
+    const maxWidth = cave.get('maxWidth');
+    let producedSand = 0;
+    let isSandLost = false;
+
+    while (!isSandLost) {
+        producedSand++;
+        let [x, y] = START;
+        let isSandMoving = true;
+
+        while (isSandMoving) {
+            if (y + 1 >= depth) {
+                isSandLost = true;
+                isSandMoving = false;
+            } else if (!cave.has(`${x}_${y + 1}`)) {
+                y++;
+            } else if (!cave.has(`${x - 1}_${y + 1}`)) {
+                y++;
+                x--;
+            } else if (!cave.has(`${x + 1}_${y + 1}`)) {
+                y++;
+                x++;
+            } else {
+                if (x >= minWidth && x <= maxWidth - 1 && y < depth - 1) {
+                    cave.set(`${x}_${y}`, SAND);
+                } else {
+                    isSandLost = true;
+                }
+                isSandMoving = false;
+            }
+        }
+    }
+
+    return producedSand - 1;
 }
 
 function day14A(file) {
@@ -30,65 +77,35 @@ function day14A(file) {
         Math,
         traces.map(trace => trace.map(([x, _]) => x)).flat()
     );
-    const width = maxWidth - minWidth + 1;
-    const cave = [...new Array(depth)].map(() =>
-        [...new Array(width)].map(() => AIR)
-    );
-    traces = traces.map(trace => trace.map(([x, y]) => [x - minWidth, y]));
+    const cave = new Map();
+    cave.set('depth', depth);
+    cave.set('minWidth', minWidth);
+    cave.set('maxWidth', maxWidth);
+    cave.set('500_0', 'x');
 
     traces.forEach(trace => {
         for (let i = 0; i < trace.length - 1; i++) {
             const from = trace[i];
             const to = trace[i + 1];
 
-            let distX = Math.abs(to[0] - from[0]);
-            let distY = Math.abs(to[1] - from[1]);
             const dirX = Math.sign(to[0] - from[0]);
             const dirY = Math.sign(to[1] - from[1]);
+            let distX = Math.abs(to[0] - from[0]);
+            let distY = Math.abs(to[1] - from[1]);
             let [x, y] = from;
+
             while (distX > 0 || distY > 0) {
-                cave[y][x] = ROCK;
+                cave.set(`${x}_${y}`, ROCK);
                 x += dirX;
                 y += dirY;
                 if (distX > 0) distX--;
                 if (distY > 0) distY--;
             }
-            cave[y][x] = ROCK;
+            cave.set(`${x}_${y}`, ROCK);
         }
     });
-    const start = [500 - minWidth, 0];
-    cave[start[1]][start[0]] = 'x';
 
-    let producedSand = 0;
-    let isSandLost = false;
-    while (!isSandLost) {
-        producedSand++;
-        let [x, y] = start;
-        let isSandMoving = true;
-        while (isSandMoving) {
-            if (cave[y + 1] === undefined) {
-                isSandLost = true;
-                isSandMoving = false;
-            } else if (cave[y + 1][x] === AIR) {
-                y++;
-            } else if (cave[y + 1][x - 1] === AIR) {
-                y++;
-                x--;
-            } else if (cave[y + 1][x + 1] === AIR) {
-                y++;
-                x++;
-            } else {
-                if (x > 0 && x < width - 1 && y < depth - 1) {
-                    cave[y][x] = SAND;
-                } else {
-                    isSandLost = true;
-                }
-                isSandMoving = false;
-            }
-        }
-    }
-
-    return producedSand - 1;
+    return getSandPouredIn({ cave });
 }
 
 module.exports = {
