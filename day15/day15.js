790cccac9d5618a6efc7a9ae100520085c9b0036
Alex Marles Alcaraz <alex.marles@king.com>
2022-12-17 16:30:55 +0100

    Day 15 - Second puzzle

--- day15/day15.js
+++ day15/day15.js
@@ -16,9 +16,25 @@ function parseSensorsFrom(data) {
         beacon.x = Number(x2.split('=')[1].replace(',', ''));
         beacon.y = Number(y2.split('=')[1]);
         sensor.radius = getTaxicabDistance({ p: sensor, q: beacon });
+        sensor.beacon = beacon;
 
         return sensor;
     });
+}
+
+function mergeIntervals(intervals) {
+    const mergedIntervals = [[...intervals[0]]];
+    for (let i = 0; i < intervals.length; i++) {
+        const current = intervals[i];
+        const previous = mergedIntervals[mergedIntervals.length - 1];
+        if (current[0] - 1 <= previous[1]) {
+            previous[1] = Math.max(previous[1], current[1]);
+        } else {
+            mergedIntervals.push(current);
+        }
+    }
+
+    return mergedIntervals;
 }
 
 function getOccupiedPositionsInRow(sensors, rowToCheck) {
@@ -33,20 +49,13 @@ function getOccupiedPositionsInRow(sensors, rowToCheck) {
                 const span = Math.abs(sensor.y - y);
                 const leftX = sensor.x - sensor.radius + span;
                 const rightX = sensor.x + sensor.radius - span;
-                if (leftX < rightX) intervals.push([leftX, rightX]);
+                if (leftX <= rightX) intervals.push([leftX, rightX]);
             }
         });
 
-    const mergedIntervals = [intervals[0]];
-    for (let i = 0; i < intervals.length; i++) {
-        const current = intervals[i];
-        const previous = mergedIntervals[mergedIntervals.length - 1];
-        if (current[0] <= previous[1]) {
-            previous[1] = Math.max(previous[1], current[1]);
-        } else {
-            mergedIntervals.push(current);
-        }
-    }
+    const mergedIntervals = mergeIntervals(
+        intervals.sort((a, b) => a[0] - b[0])
+    );
 
     return mergedIntervals;
 }
@@ -59,6 +68,34 @@ function day15A(file, rowToCheck = 10) {
     return intervals.map(i => i[1] - i[0]).reduce((acc, curr) => acc + curr, 0);
 }
 
+function day15B(file, maxCoord = 20) {
+    const data = getInputData(file);
+    const sensors = parseSensorsFrom(data);
+    const freePositions = [];
+    for (let y = 0; y < maxCoord; y++) {
+        const intervals = getOccupiedPositionsInRow(sensors, y);
+        let x = 0;
+        while (x <= maxCoord) {
+            intervals.forEach(([left, right]) => {
+                while (x < left) {
+                    freePositions.push([x, y]);
+                    x++;
+                }
+                x = right + 1;
+            });
+        }
+        while (x <= maxCoord) {
+            freePositions.push([x, y]);
+            x++;
+        }
+    }
+    const [x, y] = freePositions[0];
+    const frequency = x * 4000000 + y;
+
+    return frequency;
+}
+
 module.exports = {
     day15A,
+    day15B,
 };
--- day15/day15.test.js
+++ day15/day15.test.js
@@ -1,4 +1,4 @@
-const { day15A } = require('./day15');
+const { day15A, day15B } = require('./day15');
 
 describe('Day 15 A', () => {
     test('should solve the example', () => {
@@ -9,5 +9,17 @@ describe('Day 15 A', () => {
     test('should solve the puzzle', () => {
         const result = day15A('day15/inputData', 2000000);
         expect(result).toBe(5181556);
+    });
+});
+
+describe('Day 15 B', () => {
+    test('should solve the example', () => {
+        const result = day15B('day15/exampleData');
+        expect(result).toBe(56000011);
+    });
+
+    test('should solve the puzzle', () => {
+        const result = day15B('day15/inputData', 4000000);
+        expect(result).toBe(12817603219131);
     });
 });
