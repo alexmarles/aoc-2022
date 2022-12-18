a046737e44a750b1d9a359d8c3c4b89dc67670d6
Alex Marles Alcaraz <alex.marles@king.com>
2022-12-18 23:17:01 +0100

    WIP Day 18 - Second puzzle

--- day18/day18.js
+++ day18/day18.js
@@ -1,25 +1,104 @@
 // --- Day 18: Boiling Boulders ---
 
-const { getInputData } = require('../utils');
+const { getInputData, max } = require('../utils');
+const dijkstra = require('dijkstrajs');
 
 const CUBE_SIDES = 6;
 
-function day18A(file) {
-    const data = getInputData(file);
-    let expSides = data.length * CUBE_SIDES;
+function getContactSides(data) {
+    let contactSides = 0;
     data.forEach(c => {
         const [x, y, z] = c.split(',').map(n => Number(n));
-        if (data.includes(`${x + 1},${y},${z}`)) expSides--;
-        if (data.includes(`${x - 1},${y},${z}`)) expSides--;
-        if (data.includes(`${x},${y + 1},${z}`)) expSides--;
-        if (data.includes(`${x},${y - 1},${z}`)) expSides--;
-        if (data.includes(`${x},${y},${z + 1}`)) expSides--;
-        if (data.includes(`${x},${y},${z - 1}`)) expSides--;
+        if (data.includes(`${x + 1},${y},${z}`)) contactSides++;
+        if (data.includes(`${x - 1},${y},${z}`)) contactSides++;
+        if (data.includes(`${x},${y + 1},${z}`)) contactSides++;
+        if (data.includes(`${x},${y - 1},${z}`)) contactSides++;
+        if (data.includes(`${x},${y},${z + 1}`)) contactSides++;
+        if (data.includes(`${x},${y},${z - 1}`)) contactSides++;
     });
+    return contactSides;
+}
 
-    return expSides;
+function day18A(file) {
+    const data = getInputData(file);
+    const expSides = data.length * CUBE_SIDES;
+    return expSides - getContactSides(data);
+}
+
+function day18B(file) {
+    const data = getInputData(file);
+    let expSides = data.length * CUBE_SIDES - getContactSides(data);
+
+    const graph = {};
+    let maxX = max(data.map(l => Number(l.split(',')[0])));
+    let maxY = max(data.map(l => Number(l.split(',')[1])));
+    let maxZ = max(data.map(l => Number(l.split(',')[2])));
+    for (let x = 1; x < maxX; x++) {
+        for (let y = 1; y < maxY; y++) {
+            for (let z = 1; z < maxZ; z++) {
+                if (!data.includes(`${x},${y},${z}`)) {
+                    const neighbours = [];
+                    if (!data.includes(`${x + 1},${y},${z}`) && x < maxX) {
+                        neighbours.push(`${x + 1},${y},${z}`);
+                    }
+                    if (!data.includes(`${x - 1},${y},${z}`) && x > 1) {
+                        neighbours.push(`${x - 1},${y},${z}`);
+                    }
+                    if (!data.includes(`${x},${y + 1},${z}`) && y < maxY) {
+                        neighbours.push(`${x},${y + 1},${z}`);
+                    }
+                    if (!data.includes(`${x},${y - 1},${z}`) && y > 1) {
+                        neighbours.push(`${x},${y - 1},${z}`);
+                    }
+                    if (!data.includes(`${x},${y},${z + 1}`) && z < maxZ) {
+                        neighbours.push(`${x},${y},${z + 1}`);
+                    }
+                    if (!data.includes(`${x},${y},${z - 1}`) && z > 1) {
+                        neighbours.push(`${x},${y},${z - 1}`);
+                    }
+                    graph[`${x},${y},${z}`] = neighbours;
+                }
+            }
+        }
+    }
+
+    const pockets = new Set(Object.keys(graph));
+    const toRemove = new Set();
+
+    function markForRemoval(neighbours) {
+        neighbours.forEach(n => {
+            if (!toRemove.has(n) && pockets.has(n)) {
+                toRemove.add(n);
+                graph[n] && markForRemoval(graph[n]);
+            }
+        });
+    }
+
+    for (let key of pockets.keys()) {
+        const [x, y, z] = key.split(',').map(n => Number(n));
+        if (x <= 1 || x >= maxX || y <= 1 || y >= maxY || z <= 1 || z >= maxZ) {
+            toRemove.add(key);
+            markForRemoval(graph[key]);
+        }
+    }
+
+    // console.log({ pockets: pockets.size });
+    // console.log({ toRemove: toRemove.size });
+    [...toRemove].forEach(n => {
+        pockets.delete(n);
+    });
+    // console.log({ pockets: pockets.size });
+
+    // TODO Join points in pockets that are in the same pocket of air
+
+    const pocketSides =
+        pockets.size * CUBE_SIDES - getContactSides([...pockets]);
+    console.log({ result: expSides - pocketSides });
+
+    return expSides - pocketSides;
 }
 
 module.exports = {
     day18A,
+    day18B,
 };
--- day18/day18.test.js
+++ day18/day18.test.js
@@ -1,6 +1,6 @@
-const { day18A } = require('./day18');
+const { day18A, day18B } = require('./day18');
 
-describe('Dau 18 A', () => {
+describe('Day 18 A', () => {
     test('should solve the example', () => {
         const result = day18A('day18/exampleData');
         expect(result).toBe(64);
@@ -9,5 +9,20 @@ describe('Dau 18 A', () => {
     test('should solve the puzzle', () => {
         const result = day18A('day18/inputData');
         expect(result).toBe(3496);
+    });
+});
+
+describe('Day 18 B', () => {
+    test('should solve the example', () => {
+        const result = day18B('day18/exampleData');
+        expect(result).toBe(58);
+    });
+
+    xtest('should solve the puzzle', () => {
+        const result = day18B('day18/inputData');
+        expect(result).toBeGreaterThan(1012);
+        expect(result).not.toBe(1998);
+        expect(result).not.toBe(2034);
+        expect(result).toBeLessThan(3292);
     });
 });
